485a2e0272ad1ad61dadb52b6bf26669f95c1239
Oroz-E <Orozluis1@outlook.com>
2023-04-09 12:43:37 -0700

    task  number four

---
+++ team/luis/luis-task-4.rb
@@ -0,0 +1,31 @@
+# Generate a random integer between 0 and 100
+answer = rand(0...100)
+puts "#{answer}"
+
+i = 0 
+
+while i < 10 do
+    puts "Guess the number between 0 and 100"
+    number_guess = gets.chomp.to_i
+
+    i += 1
+
+
+    if number_guess == answer
+        puts "Number guessed!"
+        break
+    end 
+
+    if answer > number_guess
+        puts "The number is higher than #{number_guess}"
+    else 
+        puts "The number is lower than #{number_guess}"
+    end
+
+    puts "#{10-i} out of 10 attemps left"
+
+    if i == 10 && number_guess != answer 
+        puts "The number was #{answer}"
+    end
+
+end
