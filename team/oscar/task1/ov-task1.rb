1f1478825e41a4f8c69a8a4d01680b0a97d0b8a7
raczcovega <oscarvedu@gmail.com>
2023-04-09 20:40:34 -0600

    task1 commit

---
+++ team/oscar/task1/ov-task1.rb
@@ -0,0 +1,21 @@
+print "Enter a number: "
+# Read input from the user
+input = gets.chomp
+
+# Convert input to integer
+num = input.to_i
+
+# Determine if the number is positive, negative, or zero
+if num > 0
+  value = "positive"
+elsif num < 0
+  value = "negative"
+else
+  value = "No valid"
+end
+
+# Count the number of digits in the absolute value of the number
+  count = num.abs.to_s.length
+
+# Display the result
+puts "#{value} #{count}-digit"
