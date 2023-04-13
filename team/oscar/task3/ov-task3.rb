7193304af7027c6d025e8b1b824241b8fefe22cf
raczcovega <oscarvedu@gmail.com>
2023-04-13 00:20:25 -0600

    task3 fix after feedback

--- team/oscar/task3/ov-task3.rb
+++ team/oscar/task3/ov-task3.rb
@@ -3,21 +3,22 @@ print "Enter a year: "
 year = gets.chomp.to_i
 
 # Check if the year is divisible by 4
-if year % 4 == 0
-  # Check if the year is divisible by 100
-  if year % 100 == 0
-    # Check if the year is divisible by 400
-    if year % 400 == 0
-      puts "#{year} is a leap year"
-    else
-      puts "#{year} is not a leap year"
-    end
-  else
-    puts "#{year} is a leap year"
-  end
+if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+  puts "#{year} is a leap year"
 else
   puts "#{year} is not a leap year"
 end
 
-century = (year / 100) + 1
+#Calculate century by checking the residue of the given year
+
+=begin
+if year % 100 == 0
+  century = year / 100
+else
+  century = (year / 100) + 1 
+end
+=end
+century = (year / 100) + (year % 100 == 0 ? 0 : 1)
+
+#Print output
 puts "#{year} belongs to the #{century} century"
