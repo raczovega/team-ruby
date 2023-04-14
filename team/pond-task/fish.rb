abf9a324c337e630f9330162641868795de2408d
Oroz-E <Orozluis1@outlook.com>
2023-04-13 19:13:08 -0700

    classes and def order fixes

--- team/pond-task/fish.rb
+++ team/pond-task/fish.rb
@@ -1,11 +1,7 @@
 class Fish
     attr_accessor :weight
 
-    def initialize(weight)
-        @weight = rand.(1...20)
-    end
-
-    def to_s
-        "Fish (#{self.class.to_s}) with weight #{@weight}"
+    def initialize(weight = nil)
+        @weight = rand(1...20)
     end
 end
